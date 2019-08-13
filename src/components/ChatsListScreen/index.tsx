1e1b48663593cbd5faa98a6292f48f19d9862dbc
YeJ XU <nhxyj111@gmail.com>
2019-08-13 13:35:34 +0800

    init

---
+++ src/components/ChatsListScreen/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import ChatsNavbar from './ChatsNavbar';
+import ChatsList from './ChatsList';
+
+const Container = styled.div`
+  height: 100vh;
+`;
+
+const ChatsListScreen: React.FC = () => (
+  <Container>
+    <ChatsNavbar />
+    <ChatsList />
+  </Container>
+);
+
+export default ChatsListScreen;
